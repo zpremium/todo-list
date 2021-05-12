02c7af2512df7ee775b9dfbf5aaa7fb2ac135b25
zpremium <zdravkoms99@gmail.com>
2021-05-12 15:52:47 +0200

    ORGANIZACIÓN DEL PROYECTO

---
+++ js/main.js
@@ -0,0 +1,40 @@
+//1.REQUISITOS
+
+//1.1 Petición de funcionaldad (reunión 1-2h con jefe de negocio, de servicios, producto owner o project manager)
+//Tip: Apuntar todo en la reunión
+
+// -->Necesitamos una web para almacenar las tareas de los usuarios. API, TEMATIZACIÓN...
+
+// 1.2 Traducir a funcionalidades de HTML Y JS, partes de la aplicación
+// - Input con el nombre de tarea
+// - Boton para agregar tarea
+// - Listado para tareas
+// - Botón eliminar tareas
+// - Botón eliminar tareas individuales
+
+// 1.3 Definir acciones de usuario (inputs del programa)
+// - Agregar tarea
+// - Eliminar (tarea o tareas)
+
+// 1.4 Definir resultado a mostrar (API + ACCIONES USUARIOS -> OUTPUT)
+// - Lista de tareas
+
+
+/*------------------*/
+// 2. PLANIFICACIÓN
+/*------------------*/
+
+// 2.1 Hitos de la aplicación
+// - Crear un HTML estático
+// - Montar la base del javascript --> estructura & pseudocódigo.
+// - Implementar lógica JS para que sea dinámico
+// - Renellar las diferentes partes del JS
+// - Lincas & debbuggar JS+HTML
+// - Mejorar maquetación
+
+// 2.2 Tareas concretas --> ganar focus
+// - C1 [HTML] Crear input y button boostrap y dejar espacio para el listado <ul> [1h]
+// - C2 [JS] Definiar las variables que necesitará el programa
+// - C3 [JS] Definir todas las funciones vacias de js
+// - C4 [JS] Rellenar lógica de funciones una a una --> probar --> siguiente
+// - C5 [HTML] Mejorar interficie
